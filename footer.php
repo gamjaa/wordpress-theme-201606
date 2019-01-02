5aaa3110ac3f5d471a96cd494a37b2b071a8b749
정경원 <mail@gamjaa.com>
2019-01-02 22:30:14 +0900

    images 폴더 URL 수정

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 	<footer>
-		<a href="https://twitter.com/_gamjaa" target="_blank"><img src="/wp/wp-content/themes/WP_201605/images/iconmonstr-twitter-4-240.png"></a>
-		<a href="https://github.com/gamjaa" target="_blank"><img src="/wp/wp-content/themes/WP_201605/images/iconmonstr-github-1-240.png"></a>
-		<a href="/feed" target="_blank"><img src="/wp/wp-content/themes/WP_201605/images/iconmonstr-rss-feed-4-240.png"></a>
+		<a href="https://twitter.com/_gamjaa" target="_blank"><img src="<?= get_template_directory_uri() ?>/images/iconmonstr-twitter-4-240.png"></a>
+		<a href="https://github.com/gamjaa" target="_blank"><img src="<?= get_template_directory_uri() ?>/images/iconmonstr-github-1-240.png"></a>
+		<a href="/feed" target="_blank"><img src="<?= get_template_directory_uri() ?>/images/iconmonstr-rss-feed-4-240.png"></a>
 	</footer>
 
 		<?php wp_footer(); ?>
--- page-introduce.php
+++ page-introduce.php
@@ -10,7 +10,7 @@
 			<h1>포태토</h1>
 			재미있는 사람이 되고픈 진지한 사람.<br>
 			게임 개발자를 꿈꾸며, 오늘도 딴짓 중.<br><br>
-			<img src="/wp/wp-content/themes/WP_201605/images/address.png">
+			<img src="<?= get_template_directory_uri() ?>/images/address.png">
 		</div>
 </article>
 <?php get_footer(); ?>
