e72511a4ceebfba3c486c4eab8ec8a6dddeeea84
정경원 <mail@gamjaa.com>
2019-12-30 20:16:23 +0900

    애드센스 위젯 추가

--- functions.php
+++ functions.php
@@ -12,4 +12,61 @@ register_sidebar( array(
   'after_title' => '</h3>',
 ));
 add_theme_support( 'post-thumbnails' );
+add_action( 'widgets_init', function(){
+	register_widget( 'Adsense_Widget' );
+});
+
+class Adsense_Widget extends WP_Widget {
+
+	/**
+	 * Sets up the widgets name etc
+	 */
+	public function __construct() {
+		$widget_ops = array( 
+			'classname' => 'adsense_widget',
+			'description' => '애드센스 위젯',
+		);
+		parent::__construct( 'adsense_widget', 'Adsense Widget', $widget_ops );
+	}
+
+	/**
+	 * Outputs the content of the widget
+	 *
+	 * @param array $args
+	 * @param array $instance
+	 */
+	public function widget( $args, $instance ) {
+    // outputs the content of the widget
+    echo '<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+      <ins class="adsbygoogle"
+      style="display:block"
+      data-ad-client="ca-pub-9422978281782094"
+      data-ad-slot="1450533311"
+      data-ad-format="auto"></ins>
+      <script>
+      (adsbygoogle = window.adsbygoogle || []).push({});
+      </script>';
+	}
+
+	/**
+	 * Outputs the options form on admin
+	 *
+	 * @param array $instance The widget options
+	 */
+	public function form( $instance ) {
+		// outputs the options form on admin
+	}
+
+	/**
+	 * Processing widget options on save
+	 *
+	 * @param array $new_instance The new options
+	 * @param array $old_instance The previous options
+	 *
+	 * @return array
+	 */
+	public function update( $new_instance, $old_instance ) {
+		// processes widget options to be saved
+	}
+}
 ?>
--- single.php
+++ single.php
@@ -7,26 +7,8 @@
 				<span class="category"><?php the_category(', '); ?> | </span>
 				<span class="date"><?= get_the_date(); ?> <?= get_the_time(); ?></span>
 			</header>
-
+			
 			<?php
-				$tags = get_the_tags();
-				$is_contain_ad = false;
-				foreach ($tags as $tag)
-					if ($tag->name == "광고포함")
-						$is_contain_ad = true;
-
-				if ($is_contain_ad)
-					echo '<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-					<!-- 반응형 -->
-					<ins class="adsbygoogle"
-						style="display:block"
-						data-ad-client="ca-pub-9422978281782094"
-						data-ad-slot="1450533311"
-						data-ad-format="auto"></ins>
-					<script>
-					(adsbygoogle = window.adsbygoogle || []).push({});
-					</script>';
-					
 				the_content();
 				
 				the_tags('<span id="tag">태그 : ', ', ', '</span>');
@@ -37,21 +19,12 @@
 			<?php comments_template(); ?>
 		</div>
 		
-		<?php
-			if ($is_contain_ad)
-				echo '<ins class="adsbygoogle"
-				style="display:block"
-				data-ad-client="ca-pub-9422978281782094"
-				data-ad-slot="1450533311"
-				data-ad-format="auto"></ins>
-				<script>
-				(adsbygoogle = window.adsbygoogle || []).push({});
-				</script>';
-		?>
 	</div>
 
 	<nav>
 		<?php dynamic_sidebar(); ?>
+
+		<?php if (function_exists ('adinserter')) echo adinserter (2); ?>
 	</nav>
 </div>
 <?= get_footer() ?>
