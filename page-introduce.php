139affd240f6e001853b672a04e44e15729061ba
정경원 <mail@gamjaa.com>
2020-01-12 22:54:23 +0900

    소개 페이지 디자인 개선

--- page-introduce.php
+++ page-introduce.php
@@ -4,10 +4,12 @@
   */
 ?>
 <?php get_header(); ?>
-<div id="container" style="display: block">
-	<?php 
-		the_post();
-		the_content();
-	?>
+<div id="container" style="display: block; width: calc(100% - 20px); max-width: 750px;">
+	<article style="border-bottom: none;">
+		<?php 
+			the_post();
+			the_content();
+		?>
+	</article>
 </div>
 <?php get_footer(); ?>
