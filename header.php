4b4f9e83b1c884939b417f611be17cde205b3202
정경원 <mail@gamjaa.com>
2021-03-08 21:40:12 +0900

    a, code 태그 css word-break 추가

--- header.php
+++ header.php
@@ -33,7 +33,7 @@
 		<meta name="twitter:site" content="@_gamjaa">
 
 		<link rel="canonical" href="<?= is_single() ? get_the_permalink() : '' ?>">
-		<link rel="stylesheet" href="<?= get_bloginfo('stylesheet_url') ?>?ver=20210301">
+		<link rel="stylesheet" href="<?= get_bloginfo('stylesheet_url') ?>?ver=20210308">
 		<?php wp_site_icon() ?>
 		
 		<?php wp_head() ?>
